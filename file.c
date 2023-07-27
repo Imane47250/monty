caffd0a81593e50b12b6e118c505cf9da36ab865
Fatimazahralachkar <fatima6lachkar@gmail.com>
2023-07-27 02:47:01 -0700

    file func

---
+++ file.c
@@ -0,0 +1,43 @@
+#include "header.h"
+
+/**
+ * openfile -  opens the file and reads its content
+ * @filename: file name
+ * Owned by: Imane & Fatima Zahra
+ * Return: FILE
+ */
+FILE *openfile(char *filename)
+{
+	FILE *mfile;
+
+	mfile = fopen(filename, "r");
+	if (!mfile)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", filename);
+		exit(EXIT_FAILURE);
+	}
+	return (mfile);
+}
+
+
+
+/**
+ * free_stack -  frees stack
+ * @top: head of double list
+ * Return: none
+ */
+void free_stack(stack_t *top)
+{
+	stack_t *nav, *nav2;
+
+	nav = top;
+	while (nav)
+	{
+		nav2 = nav->next;
+		free(nav);
+		nav = nav2;
+	}
+	datax.top = NULL;
+	fclose(datax.mfile);
+}
+
