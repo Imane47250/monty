f465241b11de86b9b7391439e9cb2825fd2228e4
Fatimazahralachkar <fatima6lachkar@gmail.com>
2023-07-27 02:55:32 -0700

    swap func

---
+++ swap.c
@@ -0,0 +1,33 @@
+#include "header.h"
+
+/**
+ * _swap - swaps the top two elements of the stack.
+ * @top: head of double list
+ * @line_number: line number of opcode
+ * Owned by: Imane & Fatima Zahra
+ * Return: void
+ */
+void _swap(stack_t **top, unsigned int line_number)
+{
+	stack_t *tmp1;
+
+	if (!*top || (*top)->next == NULL)
+	{
+		fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
+		free_stack(datax.top);
+		exit(EXIT_FAILURE);
+	}
+	tmp1 = *top;
+	tmp1->prev = tmp1->next;
+	if ((*top)->next->next == NULL)
+		tmp1->next = NULL;
+	else
+	{
+		tmp1->next = tmp1->next->next;
+		tmp1->next->prev = *top;
+	}
+	*top = tmp1->prev;
+	(*top)->prev = NULL;
+	(*top)->next = tmp1;
+}
+
