ca0f297ac0d58f093398ac327ff3a0655c233d28
Fatimazahralachkar <fatima6lachkar@gmail.com>
2023-07-27 02:50:55 -0700

    nop func

---
+++ no_op.c
@@ -0,0 +1,18 @@
+#include "header.h"
+
+
+/**
+ * _nop - placeholder for an operation that has no effect
+ *          but does no operation on the stack.
+ * @top: head of double list
+ * @line_number: line number of opcode
+ * Owned by: Imane & Fatima Zahra
+ * Return: void
+ */
+void _nop(stack_t **top, unsigned int line_number)
+{
+	(void)top;
+	(void)line_number;
+}
+
+
